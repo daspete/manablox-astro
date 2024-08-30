90be4d10800e97db72ddb35922c0622341133684
daspete <daspetemail@gmail.com>
2024-08-30 18:29:46 +0200

    feat: init

---
+++ astro.config.mjs
@@ -0,0 +1,29 @@
+import { defineConfig, envField } from "astro/config";
+import node from "@astrojs/node";
+import tailwind from "@astrojs/tailwind";
+
+export default defineConfig({
+  devToolbar: {
+    enabled: false,
+  },
+
+  output: "server",
+
+  adapter: node({
+    mode: "standalone",
+  }),
+
+  integrations: [
+    tailwind({
+      nesting: true,
+    }),
+  ],
+
+  experimental: {
+    env: {
+      schema: {
+        API_URL: envField.string({ context: "server", access: "public" }),
+      },
+    },
+  },
+});
