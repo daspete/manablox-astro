5f5915d44e6c2c9be97ee66c39ee10c4c395f345
daspete <daspetemail@gmail.com>
2024-09-04 21:42:30 +0200

    feat: WIP graphql, primevue

--- src/vue-app.ts
+++ src/vue-app.ts
@@ -1,3 +1,10 @@
 import type { App } from 'vue'
+import PrimeVue from 'primevue/config'
+import manabloxUi from '@assets/ui/manablox-ui'
 
-export default (app: App) => {}
+export default (app: App) => {
+  app.use(PrimeVue, {
+    unstyled: true,
+    pt: manabloxUi
+  })
+}
